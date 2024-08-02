23f93d996e3011787fcbf04ce53ee02af69a1fac
Vincent Bonnet <vincent@Vincents-MBP.attlocal.net>
2024-08-01 20:32:17 -0500

    add simple mesh data for future physics engine

---
+++ src/physics/MeshData.hpp
@@ -0,0 +1,42 @@
+#ifndef Mesh_hpp
+#define Mesh_hpp
+
+#include <Eigen/Dense>
+#include <Eigen/StdVector>
+#include <vector>
+
+struct Nodes
+{
+    // Type alias for easier usage
+    using Vertex = Eigen::Vector3f;
+    using Velocity = Eigen::Vector3f;
+    using Acceleration = Eigen::Vector3f;
+    using Mass = float;
+    using VertexList = std::vector<Vertex, Eigen::aligned_allocator<Vertex>>;
+    using VelocityList = std::vector<Velocity, Eigen::aligned_allocator<Velocity>>;
+    using AccelerationList = std::vector<Acceleration, Eigen::aligned_allocator<Acceleration>>;
+    using MassList = std::vector<Mass>;
+
+    // Node attributes
+    VertexList vtx;
+    VelocityList vel;
+    AccelerationList acc;
+    MassList m;
+
+    // Constructor to initialize the mesh
+    Nodes(size_t size) : vtx(size), vel(size), acc(size), m(size ){}
+};
+
+struct TriangleConnectivity
+{
+    // Type alias for easier usage
+    using Triangle = Eigen::Vector3i;
+    using TriangleList = std::vector<Triangle, Eigen::aligned_allocator<Triangle>>;
+
+    TriangleList triangles;
+
+    // Constructor to initialize the connectivites
+    TriangleConnectivity(size_t triangleCount) : triangles(triangleCount) {}
+};
+
+#endif /* Mesh_hpp */
