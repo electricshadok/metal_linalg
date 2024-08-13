ea58a835f75d8dbc5691331047ced5037af82d6c
Vincent Bonnet <vincent@Vincents-MBP.attlocal.net>
2024-08-12 20:04:29 -0500

    update commit

--- src/physics/common.h
+++ src/physics/common.h
@@ -15,7 +15,11 @@ using ForceList = std::vector<Force, Eigen::aligned_allocator<Force>>;
 using MassList = std::vector<Mass>;
 
 // Connectivities
+using Edge = Eigen::Vector2i;
+using EdgeList = std::vector<Edge, Eigen::aligned_allocator<Edge>>;
 using Triangle = Eigen::Vector3i;
 using TriangleList = std::vector<Triangle, Eigen::aligned_allocator<Triangle>>;
+using Tet = Eigen::Vector4i;
+using TetList = std::vector<Tet, Eigen::aligned_allocator<Tet>>;
 
 #endif /* common_h */
